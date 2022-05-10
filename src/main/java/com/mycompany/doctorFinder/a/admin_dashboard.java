b21d9a3c030cadf4a10a2432aed14c403a82daf0
farah-elsayed <farahfarahat2000@gmail.com>
2022-05-10 20:37:23 +0200

    admin package and admin form

--- src/main/java/com/mycompany/doctorFinder/a/admin_dashboard.java
+++ src/main/java/com/mycompany/doctorFinder/a/admin_dashboard.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.doctorFinder.admin;
+package com.mycompany.doctorFinder.a;
 
 /**
  *
